2cc8a74d031195d66b68c0b1258dea9e9b5fd717
nicole <nicole.aretz@t-online.de>
2022-10-01 15:45:31 -0500

    added GameManager

--- Assets/Code/Runtime/MainMenu.cs
+++ Assets/Code/Runtime/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame() {
         SceneManager.LoadScene(1);
+        Debug.Log("Start button was clicked");
         EventManager.current.OnGameEnded();
     }
 
